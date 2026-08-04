85b681ed6587d42ce521bd86ee7b9c66c9c64954
ANISH <anishsihag12@gmail.com>
2026-08-04 12:07:58 +0530

    Add: reverse-bits - submission-2

---
+++ Data Structures & Algorithms/reverse-bits/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+        int i=0;
+        while(i<32){
+            ans<<=1;
+            ans=ans|(n&1);
+            
+            n>>=1;
+            i++;
+        }
+        return ans;
+    }
+};
