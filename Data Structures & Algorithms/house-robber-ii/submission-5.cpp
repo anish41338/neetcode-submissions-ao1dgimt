cabc5ad06750a5695ae4e571dd29407a94aadb60
ANISH <anishsihag12@gmail.com>
2026-08-04 19:56:31 +0530

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(vector<int>&nums,int l, int r){
+        int rob1=0;
+        int rob2=0;
+        for(int i=l;i<=r;i++){
+            int newrob=max(rob1+nums[i],rob2);
+            rob1=rob2;
+            rob2=newrob;
+        }
+        return rob2;
+    }
+    int rob(vector<int>& nums) {
+        if(nums.size()==1)return nums[0];
+        if(nums.size()==2)return max(nums[0],nums[1]);
+        return max(solve(nums,0,nums.size()-2),solve(nums,1,nums.size()-1));
+    }
+};
