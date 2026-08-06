b3388484c46cbc1cf7aa6abdb57b52c9fda594bd
ANISH <anishsihag12@gmail.com>
2026-08-07 01:10:30 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double res=1;
+        if(n<0){
+            x=1/x;
+            n=-n;
+        }
+        while(n){
+            if(n%2==1){
+                res*=x;
+
+            }
+            x*=x;
+            n/=2;
+        }
+        return res;
+    }
+};
